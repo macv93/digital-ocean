26d8131b46d54d17026b50fc9f18864e67c7e791
manny <manuel.canarte@gmail.com>
2021-02-06 18:19:56 -0500

    Add new look

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
-  <div class="flex h-screen bg-yellow-200" id="app">
-    <Nav name="manuel cañarte"/>
-
+  <div class="flex flex-col h-screen bg-black font-mono" id="app">
+      <div class="m-auto text-white">
+        <h1 class="text-center py-5">Manuel Cañarte</h1>
+        <div class="flex justify-between">
+          <a href="https://github.com/macv93" class="hover:bg-red-500 px-3" >Github</a>
+          <a href="https://www.linkedin.com/in/manuel-canarte/" class="hover:bg-red-500 px-3" >Linkedin</a>
+          <a href="https://www.instagram.com/spuddy_raspaspa/" class="hover:bg-red-500 px-3" >Instagram</a>
+        </div>
+        <img class="m-auto rounded-tl-full rounded-tr-full py-5" src="./assets/manny.png" alt="A picture of Manuel">
+        <p clas="text-right">Hola! I'm Manuel, a software dev on the path mastery</p>
+      </div>
+    <footer class="py-2 text-center text-white">️Cañarte © {{new Date().getFullYear()}}</footer>
   </div>
 </template>
 
 <script>
-import Nav from './components/Nav.vue'
 
 export default {
   name: 'App',
-  components: {
-    Nav
+  methods: {
+    reverseString(str) {
+      return str.split("").reverse().join("");
+    }
   }
-
 }
 </script>
 
