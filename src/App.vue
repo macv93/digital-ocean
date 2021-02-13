b130ed4919fb7e89259faeb746bedcaf516a10e3
manny <manuel.canarte@gmail.com>
2021-02-13 15:39:07 -0500

    Clean up tailwind and update socials

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="flex flex-col h-screen bg-black bg-opacity-75 font-mono" id="app">
     <div class="m-auto text-white">
-      <div class="flex items-center justify-center">
-        <div class="font-bold text-center text-lg text-red-600 bg-white rounded-full rounded-full border-8 border-red-600">Cañarte®</div>
+      <div class="flex items-center justify-center py-5">
+        <div class="font-bold text-center text-lg text-red-600 bg-white rounded-full border-8 border-red-600 p-1">Cañarte®</div>
       </div>
       <p class="text-center">Hola! I'm Manuel, a software dev on the path to mastery.</p>
       <img class="m-auto rounded-tl-full rounded-tr-full py-5" src="./assets/manny.png" alt="A picture of Manuel">
       <div class="flex justify-between">
-        <a href="https://blog.manuelcanarte.com/" target="_blank" class="underline hover:bg-red-500 px-3">Blog</a>
-        <a href="https://github.com/macv93" target="_blank" class="underline hover:bg-red-500 px-3">Github</a>
-        <a href="https://www.instagram.com/spuddy_raspaspa/" target="_blank" class="underline hover:bg-red-500 px-3">Instagram</a>
-        <a href="https://www.linkedin.com/in/manuel-canarte/" target="_blank" class="underline hover:bg-red-500 px-3">Linkedin</a>
+        <a href="https://blog.manuelcanarte.com/" target="_blank" class="underline hover:bg-red-500 px-3 rounded-full">Blog</a>
+        <a href="https://github.com/macv93" target="_blank" class="underline hover:bg-red-500 px-3 rounded-full">Github</a>
+        <a href="https://www.instagram.com/spuddy_raspaspa/" target="_blank" class="underline hover:bg-red-500 px-3 rounded-full">Instagram</a>
+        <a href="https://www.linkedin.com/in/manuel-canarte/" target="_blank" class="underline hover:bg-red-500 px-3 rounded-full">Linkedin</a>
       </div>
     </div>
     <footer class="py-2 text-center text-white">️Cañarte © {{ new Date().getFullYear() }}</footer>
